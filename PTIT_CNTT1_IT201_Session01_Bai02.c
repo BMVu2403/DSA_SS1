e10a8bec983ef0140d4e05431f1c1b2710af3030
BMVu2403 <buiminhvu06hn@gmail.com>
2025-06-16 22:46:13 +0700

    Update PTIT_CNTT1_IT201_Session01_Bai02.c

--- PTIT_CNTT1_IT201_Session01_Bai02.c
+++ PTIT_CNTT1_IT201_Session01_Bai02.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-void printDouble(int i) {
-    int i = 1;
-    while (i < n ) {
-        printf("%d\n",i);
-        i *=2;
+void printDouble(int n) {
+    int i = 10;
+    while (i < n) {
+        printf("%d\n", i);
+        i *= 2;
     }
     // Độ phức tạp thời gian: O(log n)
 }
 
 int main() {
-    printfDouble(20);
+    printDouble(20); // Gọi đúng tên hàm
     return 0;
 }
